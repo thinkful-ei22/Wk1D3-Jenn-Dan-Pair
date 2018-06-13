57dae5fbdf37fc0fc0e0d8415e283dfd1a8a1feb
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-13 13:19:38 -0500

    deleter drill

--- day3Drill.js
+++ day3Drill.js
@@ -44,3 +44,21 @@ function personMaker() {
 }
 
 console.log(personMaker().fullName());
+
+
+//deleting keys
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+console.log(keyDeleter(sampleObj));
